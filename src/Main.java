faf8b830313e7c48fc93aa0fc7958a761986e29e
MatveiMuruev <Matvei_Muruev@mail.ru>
2022-09-12 15:19:08 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("11110", "1"));
+        System.out.println(bins.sum("11110", "1"));
+    }
+}
